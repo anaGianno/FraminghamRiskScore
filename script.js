96bfbe3bf67cf5a5d99ac61cf2a461fadd470232
Coronado <bc245@lab-rg06-05.cms.waikato.ac.nz>
2024-08-16 12:01:34 +1200

    add js for page navigation

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let pageNumber = 1;
+
 var slider = document.getElementById("myRange");
 var output = document.getElementById("agenumber");
 output.innerHTML = slider.value; // Display the default slider value
@@ -9,6 +11,8 @@ slider.oninput = function() {
 
 function calculate(){
   var gender = document.getElementsByName('gender');
+  var age = document.getElementById("myRange").value;
+  console.log("Age: " + age);
   var smoking = document.getElementsByName('smoking');
   var totalcholesterol = document.getElementById('total-cholesterol').value;
   var hdlcholesterol = document.getElementById('hdl-cholesterol').value;
@@ -35,4 +39,62 @@ function calculate(){
 
   console.log("Total cholesterol: " + totalcholesterol);
   console.log("hdl cholesterol: " + hdlcholesterol);
+}
+
+function next(){
+  pagenumber++;
+  checkPage();
+}
+
+function restart(){
+ pagenumber = 1; 
+ checkPage();
+}
+
+function previous(){
+  pagenumber--;
+  checkPage();
+}
+
+var pageOne = document.getElementById("page-one");
+var pageTwo = document.getElementById("page-two");
+var pageThree = document.getElementById("page-three");
+var pageFour = document.getElementById("page-four");
+var pageFive = document.getElementById("page-five");
+function checkPage(){
+  if(pagenumber == 1){
+    pageOne.style.display = "block";
+    pageTwo.style.display = "none";
+    pageThree.style.display ="none";
+    pageFour.style.display= "none";
+    pageFive.style.display="none";
+  }
+  else if(pagenumber ==2){
+    pageOne.style.display("hidden");
+    pageTwo.style.display("visible");
+    pageThree.style.display("hidden");
+    pageFour.style.display("hidden");
+    pageFive.style.display("hidden");
+  }
+  else if(pagenumber ==3){
+    pageOne.style.display("hidden");
+    pageTwo.style.display("hidden");
+    pageThree.style.display("visible");
+    pageFour.style.display("hidden");
+    pageFive.style.display("hidden");
+  }
+  else if(pagenumber ==4){
+    pageOne.style.display("hidden");
+    pageTwo.style.display("hidden");
+    pageThree.style.display("hidden");
+    pageFour.style.display("visible");
+    pageFive.style.display("hidden");
+  }
+  else if(pagenumber ==5){
+    pageOne.style.display("hidden");
+    pageTwo.style.display("hidden");
+    pageThree.style.display("hidden");
+    pageFour.style.display("hidden");
+    pageFive.style.display("visible");
+  }
 }
