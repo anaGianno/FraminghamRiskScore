2bc837a708cf1e9095038b619f2d8815b8005d6b
Coronado, Benjamin (Student) <bc245@students.waikato.ac.nz>
2024-08-16 13:45:18 +1200

    add 2d array

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let pageNumber = 1;
+let ageArray = [['female',-7],['female',-3],['female',0],['female',3],['female',6],['female',8],['female',10],['female',12],['female',14],['female',16]
+                ['male',-9],['male',-4],['male',0],['male',3],['male',6],['male',8],['male',10],['male',11],['male',12],['male',13]];
 
 var slider = document.getElementById("myRange");
 var output = document.getElementById("agenumber");
@@ -39,6 +41,45 @@ function calculate(){
 
   console.log("Total cholesterol: " + totalcholesterol);
   console.log("hdl cholesterol: " + hdlcholesterol);
+
+  let ageIndex;
+  if(age<35){
+    ageIndex = 0;
+  }
+  else if(age<40){
+    ageIndex = 1;
+  }
+  else if(age<45){
+    ageIndex = 2;
+  }
+  else if(age<50){
+    ageIndex = 3;
+  }
+  else if(age<55){
+    ageIndex = 4;
+  }
+  else if(age<60){
+    ageIndex = 5;
+  }
+  else if(age<65){
+    ageIndex = 6;
+  }
+  else if(age<70){
+    ageIndex = 7;
+  }
+  else if(age<75){
+    ageIndex = 8;
+  }
+  else if(age<80){
+    ageIndex = 9;
+  }
+
+  if(gender == 'male'){
+    ageIndex += 10;
+  }
+
+
+  console.log("Output: " + );
 }
 
 function next(){
@@ -47,8 +88,8 @@ function next(){
 }
 
 function restart(){
- pagenumber = 1; 
- checkPage();
+  pagenumber = 1; 
+  checkPage();
 }
 
 function previous(){
@@ -70,31 +111,31 @@ function checkPage(){
     pageFive.style.display="none";
   }
   else if(pagenumber ==2){
-    pageOne.style.display("hidden");
-    pageTwo.style.display("visible");
-    pageThree.style.display("hidden");
-    pageFour.style.display("hidden");
-    pageFive.style.display("hidden");
+    pageOne.style.display = "none";
+    pageTwo.style.display = "block";
+    pageThree.style.display ="none";
+    pageFour.style.display= "none";
+    pageFive.style.display="none";
   }
   else if(pagenumber ==3){
-    pageOne.style.display("hidden");
-    pageTwo.style.display("hidden");
-    pageThree.style.display("visible");
-    pageFour.style.display("hidden");
-    pageFive.style.display("hidden");
+    pageOne.style.display = "none";
+    pageTwo.style.display = "none";
+    pageThree.style.display ="block";
+    pageFour.style.display= "none";
+    pageFive.style.display="none";
   }
   else if(pagenumber ==4){
-    pageOne.style.display("hidden");
-    pageTwo.style.display("hidden");
-    pageThree.style.display("hidden");
-    pageFour.style.display("visible");
-    pageFive.style.display("hidden");
+    pageOne.style.display = "none";
+    pageTwo.style.display = "none";
+    pageThree.style.display ="none";
+    pageFour.style.display= "block";
+    pageFive.style.display="none";
   }
   else if(pagenumber ==5){
-    pageOne.style.display("hidden");
-    pageTwo.style.display("hidden");
-    pageThree.style.display("hidden");
-    pageFour.style.display("hidden");
-    pageFive.style.display("visible");
+    pageOne.style.display = "none";
+    pageTwo.style.display = "none";
+    pageThree.style.display ="none";
+    pageFour.style.display= "none";
+    pageFive.style.display="block";
   }
 }
